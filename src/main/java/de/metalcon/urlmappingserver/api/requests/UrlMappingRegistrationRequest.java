454d72f1e1eb6cac07832c0c285f4a16768ff04d
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-18 16:22:33 +0100

    zero mq request inheritance

--- src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRegistrationRequest.java
+++ src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRegistrationRequest.java
@@ -1,5 +1,18 @@
 package de.metalcon.urlmappingserver.api.requests;
 
+import de.metalcon.urlmappingserver.api.requests.registration.EntityUrlData;
+
 public class UrlMappingRegistrationRequest extends UrlMappingRequest {
+
+    private EntityUrlData urlData;
+
+    public UrlMappingRegistrationRequest(
+            EntityUrlData urlData) {
+        this.urlData = urlData;
+    }
+
+    public EntityUrlData getUrlData() {
+        return urlData;
+    }
 
 }
