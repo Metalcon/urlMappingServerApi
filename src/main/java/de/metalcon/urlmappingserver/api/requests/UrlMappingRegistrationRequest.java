a0eb23c8a9cfdf1788b2deeecd07b7df21035585
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-19 14:27:00 +0100

    javaDoc

--- src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRegistrationRequest.java
+++ src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRegistrationRequest.java
@@ -2,17 +2,35 @@ package de.metalcon.urlmappingserver.api.requests;
 
 import de.metalcon.urlmappingserver.api.requests.registration.EntityUrlData;
 
+/**
+ * request: register a new URL mapping for an entity
+ * 
+ * @author sebschlicht
+ * 
+ */
 public class UrlMappingRegistrationRequest extends UrlMappingRequest {
 
     private static final long serialVersionUID = -8451776170663227973L;
 
+    /**
+     * URL information for the entity
+     */
     private EntityUrlData urlData;
 
+    /**
+     * create URL mapping registration request
+     * 
+     * @param urlData
+     *            URL information for the entity
+     */
     public UrlMappingRegistrationRequest(
             EntityUrlData urlData) {
         this.urlData = urlData;
     }
 
+    /**
+     * @return URL information for the entity
+     */
     public EntityUrlData getUrlData() {
         return urlData;
     }
--- src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRequest.java
+++ src/main/java/de/metalcon/urlmappingserver/api/requests/UrlMappingRequest.java
@@ -2,6 +2,12 @@ package de.metalcon.urlmappingserver.api.requests;
 
 import java.io.Serializable;
 
+/**
+ * basic class for URL mapping server requests
+ * 
+ * @author sebschlicht
+ * 
+ */
 public abstract class UrlMappingRequest implements Serializable {
 
     private static final long serialVersionUID = -1004857896540583557L;
