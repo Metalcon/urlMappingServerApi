1360fee0fca8ccfdfc63dff2c3b23180a5e99873
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-30 11:30:28 +0200

    [API] switched to new event URL mapping

--- src/main/java/de/metalcon/urlmappingserver/api/requests/registration/EventUrlData.java
+++ src/main/java/de/metalcon/urlmappingserver/api/requests/registration/EventUrlData.java
@@ -1,7 +1,5 @@
 package de.metalcon.urlmappingserver.api.requests.registration;
 
-import java.util.Date;
-
 import de.metalcon.domain.Muid;
 import de.metalcon.domain.MuidType;
 
@@ -16,82 +14,16 @@ public class EventUrlData extends EntityUrlData {
     private static final long serialVersionUID = -3249109237520805058L;
 
     /**
-     * first event date<br>
-     * may be <b>null</b>: unknown
-     */
-    protected Date date;
-
-    /**
-     * city the event is located in<br>
-     * may be <b>null</b>: venue set, unknown
-     */
-    protected CityUrlData city;
-
-    /**
-     * venue the event is located in<br>
-     * may be <b>null</b>: unknown
-     */
-    protected VenueUrlData venue;
-
-    /**
      * create event URL information
      * 
      * @param muid
      *            event ID
-     * @param name
-     *            event name
-     * @param date
-     *            first event date (milliseconds will be ignored), may be null
-     * @param city
-     *            city the event is located in<br>
-     *            may be <b>null</b><br>
-     *            if <b>null</b> the city of the venue is taken, if
-     *            venue was set
-     * @param venue
-     *            venue the event is located in<br>
-     *            may be <b>null</b>
      * @throws IllegalArgumentException
-     *             if MUID or name <b>null</b> or MUID type mismatching
+     *             if MUID <b>null</b> or MUID type mismatching
      */
     public EventUrlData(
-            Muid muid,
-            String name,
-            Date date,
-            CityUrlData city,
-            VenueUrlData venue) {
-        super(false, MuidType.EVENT, muid, name);
-        this.date =
-                (date != null) ? new Date(date.getTime() - date.getTime()
-                        % 1000) : null;
-        this.city = city;
-        this.venue = venue;
-    }
-
-    /**
-     * @return first event date<br>
-     *         may be <b>null</b>: unknown
-     */
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * @return city the event is located in<br>
-     *         may be <b>null</b>
-     */
-    public CityUrlData getCity() {
-        if (city == null && venue != null) {
-            return venue.getCity();
-        }
-        return city;
-    }
-
-    /**
-     * @return venue the event is located in<br>
-     *         may be <b>null</b>
-     */
-    public VenueUrlData getVenue() {
-        return venue;
+            Muid muid) {
+        super(false, MuidType.EVENT, muid);
     }
 
 }
--- src/test/java/de/metalcon/urlmappingserver/api/requests/registration/EventUrlDataTest.java
+++ src/test/java/de/metalcon/urlmappingserver/api/requests/registration/EventUrlDataTest.java
@@ -1,31 +1,13 @@
 package de.metalcon.urlmappingserver.api.requests.registration;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.util.Date;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.metalcon.domain.MuidType;
-import de.metalcon.testing.MuidFactory;
 
 public class EventUrlDataTest extends EntityUrlDataTest {
-
-    private static final long CURRENT_MS = System.currentTimeMillis();
-
-    protected static final Date VALID_DATE = new Date(CURRENT_MS - CURRENT_MS
-            % 1000);
-
-    protected static final CityUrlData VALID_CITY = CityUrlDataTest.VALID_CITY;
-
-    protected static final VenueUrlData VALID_VENUE =
-            VenueUrlDataTest.VALID_VENUE;
-
-    protected static final VenueUrlData VALID_VENUE_WITHOUR_CITY =
-            new VenueUrlData(MuidFactory.generateMuid(MuidType.VENUE),
-                    VALID_VENUE.getName(), null);
 
     protected EventUrlData event;
 
@@ -37,107 +19,35 @@ public class EventUrlDataTest extends EntityUrlDataTest {
 
     @Override
     public void testEntityNotEmpty() {
-        entity =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE,
-                        VALID_CITY, VALID_VENUE);
+        entity = new EventUrlData(VALID_MUID);
         super.testEntityNotEmpty();
     }
 
     @Override
     public void testEntityFull() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE,
-                        VALID_CITY, VALID_VENUE);
+        event = new EventUrlData(VALID_MUID);
         testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertEquals(VALID_CITY, event.getCity());
-        assertEquals(VALID_VENUE, event.getVenue());
     }
 
     @Override
     public void testMuidNull() {
-        event =
-                new EventUrlData(null, VALID_NAME, VALID_DATE, VALID_CITY,
-                        VALID_VENUE);
+        event = new EventUrlData(null);
     }
 
     @Override
     public void testMuidTypeInvalid() {
-        event =
-                new EventUrlData(INVALID_MUID, VALID_NAME, VALID_DATE,
-                        VALID_CITY, VALID_VENUE);
+        event = new EventUrlData(INVALID_MUID);
+    }
+
+    @Test
+    @Override
+    public void testNameNull() {
+        // allowed
     }
 
     @Override
-    public void testNameNull() {
-        event =
-                new EventUrlData(VALID_MUID, null, VALID_DATE, VALID_CITY,
-                        VALID_VENUE);
-    }
-
-    @Test
-    public void testDateNull() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, null, VALID_CITY,
-                        VALID_VENUE);
-        testEntityValid(event);
-        assertNull(event.getDate());
-        assertEquals(VALID_CITY, event.getCity());
-        assertEquals(VALID_VENUE, event.getVenue());
-    }
-
-    @Test
-    public void testCityNull() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE, null,
-                        VALID_VENUE);
-        testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertEquals(VALID_VENUE.getCity(), event.getCity());
-        assertEquals(VALID_VENUE, event.getVenue());
-    }
-
-    @Test
-    public void testCityNullVenueNull() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE, null, null);
-        testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertNull(event.getCity());
-        assertNull(event.getVenue());
-    }
-
-    @Test
-    public void testCityNullVenueWithoutCity() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE, null,
-                        VALID_VENUE_WITHOUR_CITY);
-        testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertNull(event.getCity());
-        assertEquals(VALID_VENUE_WITHOUR_CITY, event.getVenue());
-    }
-
-    @Test
-    public void testVenueNull() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE,
-                        VALID_CITY, null);
-        testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertEquals(VALID_CITY, event.getCity());
-        assertNull(event.getVenue());
-    }
-
-    @Test
-    public void testVenueWithoutCity() {
-        event =
-                new EventUrlData(VALID_MUID, VALID_NAME, VALID_DATE,
-                        VALID_CITY, VALID_VENUE_WITHOUR_CITY);
-        testEntityValid(event);
-        assertEquals(VALID_DATE, event.getDate());
-        assertEquals(VALID_CITY, event.getCity());
-        assertEquals(VALID_VENUE_WITHOUR_CITY, event.getVenue());
+    protected void testEntityValid(EntityUrlData entity) {
+        assertNotNull(entity.getMuid());
     }
 
 }
